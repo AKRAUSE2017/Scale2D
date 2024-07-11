c2cbbe041e12c5de2b75dd5fdd633df30f67eb31
Alena Krause <akrause2017@fau.edu>
2024-07-10 22:23:12 -0400

    trying something else

--- main.lua
+++ main.lua
@@ -1,8 +1,37 @@
 push = require('imports.push')
-
 require('helpers.constants')
 
 local player = {x=500, y=600, w=100, h=100, dx=0, dy=0, state="ground"}
+
+function love.keypressed(key)
+    if key == "space" then
+        player.dy = -2
+        player.state = "air"
+    end
+end
+
+function love.update(dt)
+    if love.keyboard.isDown("right") then
+        player.dx = 200 * dt
+    elseif love.keyboard.isDown("left") then
+        player.dx = -200 * dt
+    else
+        player.dx = 0
+    end
+
+    if player.state == "air" then
+        player.dy = player.dy + 5 * dt
+    end
+
+    if player.y > 600 then 
+        player.state = "ground"
+        player.y = 600
+        player.dy = 0
+    end
+        
+    player.x = player.x + player.dx
+    player.y = player.y + player.dy
+end
 
 function love.load()
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
@@ -12,43 +41,9 @@ function love.load()
     })
 end
 
-function love.keypressed(key)
-    if key == "space" then
-        player.state = "jump"
-    end
-end
-
 function love.draw()
     push:start() 
-
     love.graphics.setColor(255/255, 255/255, 255/255)
     love.graphics.rectangle("fill", player.x, player.y, player.w, player.h)
-
     push:finish()
-end
-
-function love.update(dt)
-    if love.keyboard.isDown('d') or love.keyboard.isDown("right") then
-        player.dx = 200 * dt
-    elseif love.keyboard.isDown('a') or love.keyboard.isDown("left") then
-        player.dx = -200 * dt
-    else
-        player.dx = 0
-    end
-
-    if player.state == "jump" then
-        player.dy = -250 * dt
-        player.state = "air"
-    elseif player.state == "air" then
-        player.dy = player.dy + 5 * dt
-    end
-
-    if player.y > 600 then 
-        player.y = 600
-        player.state = "ground"
-        player.dy = 0
-    end
-        
-    player.x = player.x + player.dx
-    player.y = player.y + player.dy
 end
